6d0784196c4d888348de5f351f2cc26648330381
MrJohnsson <christian@mrjohnsson.net>
2015-06-10 02:35:29 +0200

    Minor typos and formating changes

--- example/example.php
+++ example/example.php
@@ -1,13 +1,11 @@
-<?php 
+<?php
 require_once "../src/RobotDetector/RobotDetector.php";
-
 
 $detect = new RobotDetector\RobotDetector;
 $user_agent = $_SERVER['HTTP_USER_AGENT'];
 
 $check = $detect->RobotDetect($user_agent);
 
-$robot = ($check === 0) ? "Not a robot": "This is a robot";
+$robot = ($check === 0) ? "Not a robot" : "This is a robot";
 
 echo "$robot";
- ?>
--- tests/RobotDetector/RobotDetector.php
+++ tests/RobotDetector/RobotDetector.php
@@ -1,26 +1,26 @@
-<?php 
+<?php
 $base = realpath(dirname(__FILE__) . '/../..');
 require "$base/src/RobotDetector/RobotDetector.php";
 
-class testRobotDetector extends PHPUnit_Framework_TestCase {
+class testRobotDetector extends PHPUnit_Framework_TestCase
+{
 
-	private $test = false;
+    private $test = false;
 
-	public function SetUp() {
-		$this->test = new RobotDetector\RobotDetector;
-	}
+    public function SetUp()
+    {
+        $this->test = new RobotDetector\RobotDetector;
+    }
 
-	public function testRobotDetect()
-	{	
-        $emptyRobot = @$this->test->RobotDetect();
+    public function testRobotDetect()
+    {
         $isRobot = $this->test->RobotDetect('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)');
         $isNotRobot = $this->test->RobotDetect('Mozilla/5.0 (Windows; U; Windows NT 6.1; pl; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3');
         $CommonBrowser = $this->test->RobotDetect('Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.2 (KHTML, like Gecko) Chrome/6.0');
 
-		$this->assertTrue(($emptyRobot === 0), 'Error getting integer from Non Robot');
-		$this->assertTrue(($isRobot === 1), 'Error getting integer from Is Robot');
-		$this->assertTrue(($isNotRobot === 0), 'Error getting integer from Is Not Robot');
-		$this->assertTrue(($CommonBrowser === 0), 'Error getting integer from Common Browser');
-	}
+        $this->assertTrue(($isRobot === 1), 'Error getting integer from Is Robot');
+        $this->assertTrue(($isNotRobot === 0), 'Error getting integer from Is Not Robot');
+        $this->assertTrue(($CommonBrowser === 0), 'Error getting integer from Common Browser');
+    }
 
 }
